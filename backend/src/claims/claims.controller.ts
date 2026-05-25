20a0c2400f46f57ed43b0ed1a161ed301a83c55d
rapipp24 <rafif_danu_saputra@teknokrat.ac.id>
2026-05-25 12:03:23 +0700

    feat(backend): implementasi RolesGuard pada endpoint klaim (findAll dan updateStatus)

--- backend/src/claims/claims.controller.ts
+++ backend/src/claims/claims.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, UseGuards, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { ClaimsService } from './claims.service';
 import { CreateClaimDto } from './dto/create-claim.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('claims')
 @UseGuards(AuthGuard('jwt'))
@@ -14,14 +24,16 @@ export class ClaimsController {
   }
 
   @Get()
+  @UseGuards(RolesGuard)
   findAll(
     @Query('page') page: string = '1',
-    @Query('limit') limit: string = '10'
+    @Query('limit') limit: string = '10',
   ) {
     return this.claimsService.findAll(+page, +limit);
   }
 
   @Patch(':id/status')
+  @UseGuards(RolesGuard) //admin only
   updateStatus(@Param('id') id: string, @Body('status') status: string) {
     return this.claimsService.updateStatus(+id, status);
   }
