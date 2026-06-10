279d846ef11371f6d60d7e37acc465ed6a2542a9
wandaVbyaAA <anantaw08@gmail.com>
2026-06-10 15:18:06 +0700

    fix(frontend):memperbaiki syntax error dan menambahkan nilai cadangan (fallback) pada file next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.BACKEND_URL}/:path*`,
+        destination: `${process.env.BACKEND_URL || "http://localhost:3000"}/:path*`,
       },
     ];
   },
