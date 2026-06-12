59af3ceaf5894babff1bf6a061a65139f69687f1
rapipp24 <rafif_danu_saputra@teknokrat.ac.id>
2026-06-13 00:21:22 +0700

    fix(cors): mengizinkan dynamic origin untuk integrasi Flutter Web

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,8 +22,7 @@ async function bootstrap() {
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: true,
     credentials: true,
   });
 
